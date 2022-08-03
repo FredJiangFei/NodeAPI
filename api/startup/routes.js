f560f4057da11dd04006515b935260c01ce70da0
Fred Jiang <jiangf@shinetechsoftware.com>
2022-08-03 15:15:21 +0800

    add test

--- api/startup/routes.js
+++ api/startup/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const products = require('../routes/products');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
+const test = require('../routes/test');
 const error = require('../middleware/error');
 
 module.exports = function(app) {
@@ -9,5 +10,6 @@ module.exports = function(app) {
   app.use('/api/products', products);
   app.use('/api/users', users);
   app.use('/api/auth', auth);
+  app.use('/api/test', test);
   app.use(error);
 }
